83ae453b4e407837468664adc3fb14a665b1039e
LeeTaeSoon <nrst136@naver.com>
2017-05-26 03:30:35 +0900

    Root layout -> DrawerLayout and side menu fragment
    Change root layout as DrawerLayout to use side menu bar.
    
    fragment is encased in FrameLayout. It just contain the fragment because
    can't change attribute of fragment.
    
    But I want to use menu button and back button in android phone. These
    buttons help to open and close side menu. So I need to View object, to
    use container View.

--- app/src/main/java/com/example/nrst1/scheduleguide/MainActivity.java
+++ app/src/main/java/com/example/nrst1/scheduleguide/MainActivity.java
@@ -2,21 +2,29 @@ package com.example.nrst1.scheduleguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     GridView calanderGrid;
+    FrameLayout sideMenuContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        sideMenuContainer = (FrameLayout) findViewById(R.id.side_bar_fragment_container);
+
         ActionBarHandler actionBarHandler = new ActionBarHandler(this, getSupportActionBar());
         actionBarHandler.setBasicActionBar();
+        actionBarHandler.setDrawerMenu(drawerLayout, sideMenuContainer);
 
         ArrayList<Day> days = new ArrayList<>();
         for(int i = 1 ; i < 32; i++) {
@@ -27,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
         calanderGrid = (GridView) findViewById(R.id.grid_calander);
         DayAdapter dayAdapter = new DayAdapter(this, days);
         calanderGrid.setAdapter(dayAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(sideMenuContainer)) drawerLayout.closeDrawer(sideMenuContainer);
+        else super.onBackPressed();
     }
 }
