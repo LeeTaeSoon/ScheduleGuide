2d92df6f0f50bb6701c5f1a881c301dfd10798e7
LeeTaeSoon <nrst136@naver.com>
2017-05-26 03:32:17 +0900

    Add two Event handler in Actionbar
    1. go to main activity to click home image
    
    2. open / close side menu to click menu button

--- app/src/main/java/com/example/nrst1/scheduleguide/ActionBarHandler.java
+++ app/src/main/java/com/example/nrst1/scheduleguide/ActionBarHandler.java
@@ -1,10 +1,12 @@
 package com.example.nrst1.scheduleguide;
 
 import android.content.Context;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -36,6 +38,25 @@ public class ActionBarHandler {
 
         Toolbar parent = (Toolbar) customView.getParent();
         parent.setContentInsetsAbsolute(0, 0);
+
+        ImageView homeButton = (ImageView) customView.findViewById(R.id.actionBarLogo);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : 메인화면으로 이동
+            }
+        });
+    }
+
+    public void setDrawerMenu(final DrawerLayout drawerLayout, final View menuView) {
+        ImageView menuButton = (ImageView) customView.findViewById(R.id.actionBarMenu);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(menuView)) drawerLayout.closeDrawer(menuView);
+                else drawerLayout.openDrawer(menuView);
+            }
+        });
     }
 
     public void setTitle(String title) {
