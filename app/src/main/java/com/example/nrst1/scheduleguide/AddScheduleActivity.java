c533fa22a2f2be4936cf9bcc6c70765e447545c8
AhnJeonghyeon <ajh794@naver.com>
2017-05-30 18:17:52 +0900

    일정추가페이지
    시작페이지에서 년월일 가져오고 받은 다음 ui들 초기화 했습니다.

--- app/src/main/java/com/example/nrst1/scheduleguide/AddScheduleActivity.java
+++ app/src/main/java/com/example/nrst1/scheduleguide/AddScheduleActivity.java
@@ -1,21 +1,38 @@
 package com.example.nrst1.scheduleguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddScheduleActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     FrameLayout sideMenuContainer;
 
+    int year,month,day;
+    Spinner tag;
+    EditText title;
+    Spinner alarm;
+    EditText location;
+    EditText attend;
+    EditText memo;
+
+    TextView startTime;
+    TextView startDate;
+    TextView endTime;
+    TextView endDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
 
         initActionBar();
+        init();
     }
 
     public void initActionBar() {
@@ -27,10 +44,33 @@ public class AddScheduleActivity extends AppCompatActivity {
         actionBarHandler.setTitle("일정 추가");
         actionBarHandler.setDrawerMenu(drawerLayout, sideMenuContainer);
     }
-
+//tag가져오기
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(sideMenuContainer)) drawerLayout.closeDrawer(sideMenuContainer);
         else super.onBackPressed();
+    }
+
+    public void init(){
+        Intent intent=getIntent();
+        year=intent.getIntExtra("year",-1);
+        month=intent.getIntExtra("month",-1);
+        day=intent.getIntExtra("day",-1);
+        //intent를 이용해 시작 페이지에서 년 월 일을 가져왔다
+        //이제 db에서 이 사람의 이 날짜의 일정과 태그를 가져온다
+        tag=(Spinner)findViewById(R.id.add_schedule_tag);
+        title=(EditText)findViewById(R.id.add_schedule_title);
+        alarm=(Spinner)findViewById(R.id.add_schedule_alarm);
+        location=(EditText)findViewById(R.id.add_schedule_location);
+        attend=(EditText)findViewById(R.id.add_schedule_attendance);
+        memo=(EditText)findViewById(R.id.add_schedule_memo);
+
+        startDate=(TextView)findViewById(R.id.add_schedule_start_date);
+        startTime=(TextView)findViewById(R.id.add_schedule_start_time);
+        endDate=(TextView)findViewById(R.id.add_schedule_end_date);
+        endTime=(TextView)findViewById(R.id.add_schedule_end_time);
+
+
+
     }
 }
--- app/src/main/java/com/example/nrst1/scheduleguide/MainActivity.java
+++ app/src/main/java/com/example/nrst1/scheduleguide/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.nrst1.scheduleguide;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -17,19 +18,55 @@ public class MainActivity extends AppCompatActivity {
     GridView calanderGrid;
     FrameLayout sideMenuContainer;
 
+    Spinner spinner_year;
+    Spinner spinner_month;
+    int year;
+    int month;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+        initActionBar();
+    }
+
+    public void initActionBar(){
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         sideMenuContainer = (FrameLayout) findViewById(R.id.side_bar_fragment_container);
 
         ActionBarHandler actionBarHandler = new ActionBarHandler(this, getSupportActionBar());
         actionBarHandler.setBasicActionBar();
         actionBarHandler.setDrawerMenu(drawerLayout, sideMenuContainer);
+    }
 
-        ArrayList<Day> days = new ArrayList<>();
+    public void init(){
+        spinner_year=(Spinner)findViewById(R.id.year);
+        spinner_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                year=position+2017;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                year=2017;
+            }
+        });
+        spinner_month=(Spinner)findViewById(R.id.month);
+        spinner_month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                month=position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                month=6;
+            }
+        });
+        ArrayList<Day> days = new ArrayList<>();            //여기서 day계산이 필요하다
         for(int i = 1 ; i < 32; i++) {
             Day day = new Day(2017, 5, i, i % 7);
             days.add(day);
@@ -39,11 +76,15 @@ public class MainActivity extends AppCompatActivity {
         DayAdapter dayAdapter = new DayAdapter(this, days);
         calanderGrid.setAdapter(dayAdapter);
 
-        // TODO : 선택한 날짜에 일정이 있으면 일정 목록 페이지로 이동하도록 분기 처리 
+        // TODO : 선택한 날짜에 일정이 있으면 일정 목록 페이지로 이동하도록 분기 처리
         calanderGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //여기서 그리드뷰의 position은 1번째가 0부터 시작한다
                 Intent intent = new Intent(getApplication(), AddScheduleActivity.class);
+                intent.putExtra("year",year);
+                intent.putExtra("month",month);
+                intent.putExtra("day",position);        //포지션은 계산하겠지
                 startActivity(intent);
             }
         });
