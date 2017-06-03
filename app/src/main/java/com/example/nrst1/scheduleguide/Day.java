c3431b6f9922e86e86e8dba2ed497b5b6f253cd9
LeeTaeSoon <nrst136@naver.com>
2017-06-04 00:12:22 +0900

    Calcurate day of the week
    input : year, month ,day
    output : day of the week
    
    ******
    Do not use this funtion because of time, async problem
    
    Use Calendar or SimpleDataFormat

--- app/src/main/java/com/example/nrst1/scheduleguide/Day.java
+++ app/src/main/java/com/example/nrst1/scheduleguide/Day.java
@@ -1,5 +1,13 @@
 package com.example.nrst1.scheduleguide;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by nrst1 on 2017-05-25.
  */
@@ -26,4 +34,36 @@ public class Day {
     int getMonth() { return this.month; }
     int getDay() { return this.day; }
     int getDayOfTheWeek() { return this.dayOfTheWeek; }
+
+    public int getDayOfTheWeek(int year, int month, int day) {
+        FirebaseHandler firebaseHandler = new FirebaseHandler();
+        firebaseHandler.setFirebase();
+        DatabaseReference calenderTable = firebaseHandler.getCalanderTable();
+
+        final int[] first = {-1};
+
+        calenderTable.child(String.valueOf(year)).child(String.valueOf(month)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try {
+                    JSONObject day = new JSONObject(dataSnapshot.toString());
+                    first[0] = day.getInt(String.valueOf("1"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        while (first[0] == -1);
+
+        int dayOfTheWeek = day - 1;
+        dayOfTheWeek = dayOfTheWeek + first[0] % 7;
+
+        return dayOfTheWeek;
+    }
 }
