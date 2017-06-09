bfd0d08a77a152d168544d2525892383461fe9fb
LeeTaeSoon <nrst136@naver.com>
2017-06-09 11:36:02 +0900

    Fix alarm time error

--- app/src/main/java/com/example/nrst1/scheduleguide/UpdateSchedule.java
+++ app/src/main/java/com/example/nrst1/scheduleguide/UpdateSchedule.java
@@ -210,7 +210,7 @@ public class UpdateSchedule extends AppCompatActivity {
         alarm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                ringring=(position+1)*0.5;//분
+                ringring = position * 0.5;
             }
 
             @Override
